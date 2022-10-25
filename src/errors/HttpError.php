428a13ea0e5c6e752c6a41af457c16a4cbd21f2c
Rudi Ullón <scretch@gmail.com>
2022-10-25 16:10:11 -0300

    HTTP ERROR FIX

--- src/errors/HttpError.php
+++ src/errors/HttpError.php
@@ -4,9 +4,9 @@ namespace Onfleet\errors;
 
 class HttpError extends \Exception
 {
-	private $_name = 'HttpError';
+	private  $_name = 'HttpError';
 
-	public function __construct(string $message = "", string $cause = "", int $code = 0, $request = "")
+	public function __construct(string $message = "", string $cause = "", int $code = 0, mixed $request = null)
 	{
 		parent::__construct("[{$this->_name}] {$message} {$cause}.", $code);
 	}
